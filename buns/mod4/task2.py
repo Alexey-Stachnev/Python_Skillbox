036a714dc56fdfc0f022d8dcc110004f08bf9629
Alexey Stachnev <129387109+Alexey-Stachnev@users.noreply.github.com>
2023-12-08 20:47:38 +0500

    Delete buns/mod4 directory

--- buns/mod4/task2.py
+++
@@ -1,9 +0,0 @@
-def exponentiation(a, n):
-    if n == 0:
-        return 1
-    if n % 2 == 0:
-        return exponentiation(a**2, n/2)
-    else:
-        return a * exponentiation(a, n-1)
-
-print(exponentiation(10,2))
--- buns/mod4/task3.py
+++
@@ -1,4 +0,0 @@
-def gcd(num1, num2):
-    if num1 == 0:
-        return num2
-    return gcd(num2 % num1, num1)
