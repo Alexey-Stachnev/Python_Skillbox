af4dd08f22a36be804f5f355a753424466c2ba84
Alexey Stachnev <129387109+Alexey-Stachnev@users.noreply.github.com>
2023-12-08 22:51:59 +0500

    Update task.1.py

--- buns/mod5/task.1.py
+++ buns/mod5/task.1.py
@@ -1,18 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class Stack:
     def __init__(self):
-        self.stack = []
-    def push(self, item):
-        self.stack.append(item)
+        self.__head = None
+
+    def push(self, value):
+        if self.__head is None:
+            self.__head = Node(value)
+        else:
+            newNode = Node(value)
+            newNode.next = self.__head
+            self.__head = newNode
+
     def pop(self):
         if self.is_empty():
             return None
-        removed = self.stack.pop()
+        removed = self.__head
+        self.__head = self.__head.next
+        removed.next = None
         return removed
-    def top(self):
+
+    def is_empty(self):
+        return self.__head is None
+
+    def peek(self):
         if self.is_empty():
             return None
-        return self.stack[len(self.stack)-1]
-    def is_empty(self):
-        return len(self.stack) == 0
-    def size(self):
-        return len(self.stack)
+        return self.__head.value
+
+    def print_stack(self):
+        if self.is_empty():
+            print("Stack Underflow")
+        else:
+            node = self.__head
+            while(node is not None):
+                print(node.value, end = "\n")
+                node = node.next
